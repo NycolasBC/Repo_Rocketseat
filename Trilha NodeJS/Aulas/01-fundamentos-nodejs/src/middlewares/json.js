46d28bba6f3a261efaf37b22e919782a73c32e4b
NycolasBC <nycolascarvalho14@gmail.com>
2023-03-27 09:10:09 -0300

    Pasta rocketseat

---
+++ Trilha NodeJS/Aulas/01-fundamentos-nodejs/src/middlewares/json.js
@@ -0,0 +1,17 @@
+
+
+export async function json(request, response) {
+    const buffers = []
+
+    for await (const chunk of request) {
+        buffers.push(chunk)
+    }
+
+    try {
+        request.body = JSON.parse(Buffer.concat(buffers).toString())
+    } catch {
+        request.body = null
+    }
+
+    response.setHeader('Content-type', 'application/json')
+}
---
+++ Trilha NodeJS/Aulas/01-fundamentos-nodejs/streams/stream-http-server.js
@@ -0,0 +1,33 @@
+import http from 'node:http'
+import { Transform } from 'node:stream'
+
+
+class InverseNumberStream extends Transform {
+    _transform(chunk, enconding, callback) {
+        const transformed = Number(chunk.toString()) * -1
+
+        console, console.log((transformed))
+
+        callback(null, Buffer.from(String(transformed)))
+    }
+}
+
+
+const server = http.createServer(async (req, res) => {
+    const buffers = []
+
+    for await (const chunk of req) {
+        buffers.push(chunk)
+    }
+
+    const fullStreamContent = Buffer.concat(buffers).toString()
+
+    console.log(fullStreamContent)
+
+    return res.end(fullStreamContent)
+    // return req
+    //     .pipe(new InverseNumberStream)
+    //     .pipe(res)
+})
+
+server.listen(3334)
